afcfd69c7497f8e05de3eaa9b704af17964e3377
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-02-23 16:57:45 +0100

    fix Cart.update totalPrice

--- dist/js/script.js
+++ dist/js/script.js
@@ -388,7 +388,7 @@ const select = {
       thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
       thisCart.dom.deliveryFee = thisCart.dom.wrapper.querySelector(select.cart.deliveryFee);
       thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
-      thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelector(select.cart.totalPrice);
+      thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
       thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
       thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
       thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
@@ -440,7 +440,7 @@ const select = {
       thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
       thisCart.totalNumber = 0;
       thisCart.subtotalPrice = 0;
-      // thisCart.totalPrice = 0;
+      thisCart.totalPrice = 0;
 
       for(let product of thisCart.products){
         thisCart.totalNumber += product.amount;
@@ -461,8 +461,11 @@ const select = {
       thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
       thisCart.dom.deliveryFee.innerHTML = thisCart.deliveryFee;
       thisCart.dom.subtotalPrice.innerHTML = thisCart.subtotalPrice;
-      thisCart.dom.totalPrice.innerHTML = thisCart.totalPrice;
+      //thisCart.dom.totalPrice.innerHTML = thisCart.totalPrice;
       
+       for(let totalPrice of thisCart.dom.totalPrice){
+        totalPrice.innerHTML = thisCart.totalPrice;
+       }
 
       console.log('deliveryFee:', thisCart.deliveryFee, 'totalPrice:', thisCart.totalPrice);
     }
